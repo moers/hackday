0acb1ecf6b4674c16d5ac9ccd7d39796db679811
Elmar Burke <elmar.burke@symetics.com>
2015-03-19 10:05:39 +0100

    hashtag

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
 
 $(document).ready(function() {
 
-	$("body").addClass("loaded");
+  $('body').addClass('loaded');
 
-	$(function () {
-		$('a[rel="lightbox"]').fluidbox();
-	})
+  $(function() {
+    $('a[rel="lightbox"]').fluidbox();
+  });
+
+  function fixDiv() {
+    var $div = $('#hashtag');
+    var $p = $('#hashtag p');
+    var top = $div.data('top');
+    if ($(window).scrollTop() > top) {
+      $div.css({
+        position: 'fixed',
+        top: 0
+      });
+      $p.css({
+        'padding-top': 0,
+        'font-size': '32px'
+      });
+    } else {
+      $div.css({
+        position: 'block',
+        top: 'auto'
+      });
+      $p.css({
+        'padding-top': '0.5em',
+        'font-size': '48px'
+      });
+    }
+  }
+
+  $('#hashtag').data('top', $('#hashtag').offset().top); // set original position on load
+  $(window).scroll(fixDiv);
 
 });
